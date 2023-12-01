b3f7f270453faa286548e82c88acd58ffafaf420
Anggarez <136096183+Anggarez@users.noreply.github.com>
2023-12-01 11:45:37 +0700

    nn

--- handler/handler.js
+++ handler/handler.js
@@ -3,137 +3,138 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const Company = require('../model/companyModel');
-
+const jwt = require('jsonwebtoken');
 
 
 const registerCompany = async (req, res) => {
-  try {
-    const { namaCompany, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+        const { namaCompany, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    const newCompany = new Company({
-      namaCompany,
-      kodeReferral: generateReferralCode(),
-      email,
-      password: hashedPassword,
-    });
-    await newCompany.save();
-    res.status(201).json({ message: 'Akun perusahaan berhasil dibuat' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Terjadi kesalahan' });
-  }
+        const newCompany = new Company({
+            namaCompany,
+            kodeReferral: generateReferralCode(),
+            email,
+            password: hashedPassword,
+        });
+        await newCompany.save();
+        res.status(201).json({ message: 'Akun perusahaan berhasil dibuat' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Terjadi kesalahan' });
+    }
 };
 
 const registerUser = async (req, res) => {
-  try {
-    const { nama, email, password, kodeReferral } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+        const { nama, email, password, kodeReferral } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-    if (kodeReferral) {
-      const company = await Company.findOne({ kodeReferral });
+        if (kodeReferral) {
+            const company = await Company.findOne({ kodeReferral });
 
-      if (!company) {
-        return res.status(400).json({ message: 'Kode Referral tidak valid' });
-      }
+            if (!company) {
+                return res.status(400).json({ message: 'Kode Referral tidak valid' });
+            }
 
-      const newUser = new User({
-        nama,
-        email,
-        password: hashedPassword,
-        kodeReferral,
-      });
+            const newUser = new User({
+                nama,
+                email,
+                password: hashedPassword,
+                kodeReferral,
+            });
 
-      await newUser.save();
-      return res.status(201).json({ message: 'Akun pengguna berhasil dibuat dan terhubung dengan perusahaan' });
-    } else {
-      const newUser = new User({
-        nama,
-        email,
-        password: hashedPassword,
-      });
+            await newUser.save();
+            return res.status(201).json({ message: 'Akun pengguna berhasil dibuat dan terhubung dengan perusahaan' });
+        } else {
+            const newUser = new User({
+                nama,
+                email,
+                password: hashedPassword,
+            });
 
-      await newUser.save();
-      return res.status(201).json({ message: 'Akun pengguna berhasil dibuat' });
+            await newUser.save();
+            return res.status(201).json({ message: 'Akun pengguna berhasil dibuat' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Terjadi kesalahan' });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Terjadi kesalahan' });
-  }
 };
 
 const login = async (req, res) => {
-  try {
-    const { email, password, role } = req.body;
+    try {
+        const { email, password, role } = req.body;
 
-    if (role === 'company') {
-      const company = await Company.findOne({ email });
+        if (role === 'company') {
+            const company = await Company.findOne({ email });
 
-      if (company) {
-        const passwordMatch = await bcrypt.compare(password, company.password);
+            if (company) {
+                const passwordMatch = await bcrypt.compare(password, company.password);
 
-        if (passwordMatch) {
-          res.status(200).json({ message: 'Login sebagai perusahaan berhasil' });
+                if (passwordMatch) {
+                    const token = jwt.sign();
+                    res.status(200).json({ message: 'Login sebagai perusahaan berhasil' });
+                } else {
+                    res.status(401).json({ message: 'Email atau password salah untuk perusahaan' });
+                }
+            } else {
+                res.status(401).json({ message: 'Email atau password salah untuk perusahaan' });
+            }
+        } else if (role === 'user') {
+            const user = await User.findOne({ email });
+
+            if (user) {
+                const passwordMatch = await bcrypt.compare(password, user.password);
+
+                if (passwordMatch) {
+                    res.status(200).json({ message: 'Login sebagai pengguna berhasil' });
+                } else {
+                    res.status(401).json({ message: 'Email atau password salah untuk pengguna' });
+                }
+            } else {
+                res.status(401).json({ message: 'Email atau password salah untuk pengguna' });
+            }
         } else {
-          res.status(401).json({ message: 'Email atau password salah untuk perusahaan' });
+            res.status(400).json({ message: 'Role tidak valid' });
         }
-      } else {
-        res.status(401).json({ message: 'Email atau password salah untuk perusahaan' });
-      }
-    } else if (role === 'user') {
-      const user = await User.findOne({ email });
-
-      if (user) {
-        const passwordMatch = await bcrypt.compare(password, user.password);
-
-        if (passwordMatch) {
-          res.status(200).json({ message: 'Login sebagai pengguna berhasil' });
-        } else {
-          res.status(401).json({ message: 'Email atau password salah untuk pengguna' });
-        }
-      } else {
-        res.status(401).json({ message: 'Email atau password salah untuk pengguna' });
-      }
-    } else {
-      res.status(400).json({ message: 'Role tidak valid' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Terjadi kesalahan' });
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Terjadi kesalahan' });
-  }
 };
 
 const getUserbyReferral = async (req, res) => {
-  try {
-    const { kodeReferral } = req.body; // Gantilah ini dengan cara Anda mendapatkan kodeReferral
+    try {
+        const { kodeReferral } = req.body; // Gantilah ini dengan cara Anda mendapatkan kodeReferral
 
-    // Pastikan perusahaan yang sedang login memiliki kodeReferral yang sesuai
-    const company = await Company.findOne({ kodeReferral });
+        // Pastikan perusahaan yang sedang login memiliki kodeReferral yang sesuai
+        const company = await Company.findOne({ kodeReferral });
 
-    if (!company) {
-      return res.status(400).json({ message: 'Perusahaan tidak ditemukan atau tidak memiliki hak akses' });
+        if (!company) {
+            return res.status(400).json({ message: 'Perusahaan tidak ditemukan atau tidak memiliki hak akses' });
+        }
+
+        // Gunakan metode pada model Company untuk mendapatkan pengguna dengan kode referral tertentu
+        const users = await company.getUsersByReferral();
+
+        res.status(200).json({ message: 'Data pengguna ditemukan', users });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Terjadi kesalahan' });
     }
-
-    // Gunakan metode pada model Company untuk mendapatkan pengguna dengan kode referral tertentu
-    const users = await company.getUsersByReferral();
-
-    res.status(200).json({ message: 'Data pengguna ditemukan', users });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Terjadi kesalahan' });
-  }
 };
 
 function generateReferralCode(length = 6) {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  let referralCode = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let referralCode = '';
 
-  for (let i = 0; i < length; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.length);
-    referralCode += characters.charAt(randomIndex);
-  }
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        referralCode += characters.charAt(randomIndex);
+    }
 
-  return referralCode;
+    return referralCode;
 }
 
 module.exports = { registerCompany, registerUser, login, getUserbyReferral };
