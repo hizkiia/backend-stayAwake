d25b137b6e4d44dd9bb90946bbf4f1c8fc3fcf5f
hizkiia <109422084+hizkiia@users.noreply.github.com>
2023-12-09 20:37:19 +0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const handler = require('../handler/handler');
 
 router.post('/registerUser', handler.registerUser);
 router.post('/registerCompany', handler.registerCompany);
-router.get('/getUser', handler.getUserbyReferral);
+router.post('/getUser', handler.getUserbyReferral);
 router.post('/login', handler.login);
 
 module.exports = router;
